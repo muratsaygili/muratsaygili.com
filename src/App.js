54f4c4ab922cdade35684d23b5b9ae7b6dcd38dc
Murat Saygılı <murat@jbi.com.tr>
2022-11-20 21:29:35 +0300

    pp and social links added

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import Footer from "components/Footer";
 function App() {
   return (
     <div className="transition duration-200 dark:bg-zinc-900">
-      <div class="flex flex-col h-screen md:container md:mx-auto gap-4">
+      <div className="flex flex-col h-screen md:container md:mx-auto gap-4">
         <div className="bg-zinc-200 dark:bg-zinc-800 dark:border dark:border-zinc-700 rounded-b-3xl">
           <Header />
         </div>
-        <div class="flex-grow">
-          <div class="flex flex-col h-full md:flex-row gap-4">
-            <div class="bg-zinc-200 dark:bg-zinc-800 dark:border dark:border-zinc-700 rounded-3xl md:w-1/3 p-5">
+        <div className="flex-grow">
+          <div className="flex flex-col h-full md:flex-row gap-4">
+            <div className="bg-zinc-200 dark:bg-zinc-800 dark:border dark:border-zinc-700 rounded-3xl md:w-1/3 p-5">
               <LeftSide />
             </div>
-            <div class="bg-zinc-200 dark:bg-zinc-800 dark:border dark:border-zinc-700 rounded-3xl md:w-2/3 p-5">
+            <div className="bg-zinc-200 dark:bg-zinc-800 dark:border dark:border-zinc-700 rounded-3xl md:w-2/3 p-5">
               <RightSide />
             </div>
           </div>
--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -1,23 +1,45 @@
-import pp from "images/pp.jpg";
+import { FaLinkedin,FaGithub,FaCoffee } from "react-icons/fa";
 
 export default function LeftSide() {
-  const mt = {
-    marginTop: 50,
-  };
-  pp =
-    "https://pbs.twimg.com/profile_images/1491076055075102723/oe6YBIQC_400x400.jpg";
   return (
     <div className="justify-center">
-      <div className="flex space-x-2" style={mt}>
+      <div className="flex space-x-2 mt-12">
         <div className="relative mx-auto ">
           <img
             className="pp rounded-full border border-gray-100 shadow-sm ring-8 ring-gray-300 dark:ring-gray-500"
-            src={pp}
+            src="/images/pp.jpg"
             alt="Murat Saygılı"
           />
           {/* <div class="absolute top-0 right-0 h-16 w-16 my-1 border-4 border-white rounded-full bg-green-400 z-2"></div> */}
         </div>
-        
+      </div>
+
+      <div className="text-center mt-6">
+        <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
+          Murat Saygılı
+        </h1>
+        <p className="text-gray-500 dark:text-gray-400">Computer Engineer <FaCoffee className="inline" /></p>
+      </div>
+      <div>
+        <div className="flex justify-center mt-6">
+          <a
+            href="https://www.linkedin.com/in/muratsaygili/"
+            target="_blank"
+            rel="noreferrer"
+            className="flex items-center justify-center w-10 h-10 mr-2 text-gray-500 transition-colors duration-200 transform bg-gray-100 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 dark:bg-gray-800 dark:text-gray-100"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/muratsaygili"            
+            target="_blank"
+            rel="noreferrer"
+            className="flex items-center justify-center w-10 h-10 mr-2 text-gray-500 transition-colors duration-200 transform bg-gray-100 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 dark:bg-gray-800 dark:text-gray-100"
+          >
+            <FaGithub />
+          </a>
+
+        </div>
       </div>
     </div>
   );
