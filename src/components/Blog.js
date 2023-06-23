4d2d93af8f0237bf12886abec1e96df11df76c51
Murat Saygılı <murat@jbi.com.tr>
2023-06-24 02:15:38 +0300

    blog component fix

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,25 +5,121 @@ export default function Blog({ blog, profile }) {
   const haveBlogs = () => {
     if (blog.item) {
       return blog.item.map((post, index) => (
-        <div className="p-4 m-4 rounded-lg border border-zinc-500 dark:text-zinc-400 dark:bg-zinc-800 shadow-zinc-300 dark:shadow-zinc-600" key={index}>
-            <div className="card-image to-black w-24 h-24" style={{backgroundImage:`url(${post.thumbnail})`}}>
-                <div className="authorImage">
-                    <a href={profile.profileUrl} rel="noopener noreferrer" target="_blank" style={{backgroundImage:`url(${profile.profileImage})`}}></a>
+        <>
+          <div
+            className="p-4 m-4 max-w-sm rounded-lg border border-zinc-500 dark:text-zinc-400 dark:bg-zinc-800 shadow-zinc-300 dark:shadow-zinc-600"
+            key={index}
+          >
+            <div className="flex items-center justify-center mb-8">
+              <div className="relative">
+                <a href={post.link} rel="noopener noreferrer" target="_blank">
+                  <img
+                    src={post.thumbnail}
+                    alt="Resim 1"
+                    className="h-56 w-full object-cover"
+                  />
+                </a>
+                <div className="absolute bottom-0 left-0">
+                  <a
+                    href={profile.profileUrl}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      src={profile.profileImage}
+                      alt="Resim 2"
+                      className="w-11 h-11 rounded-full border-2 border-white -mb-3 ml-3"
+                    />
+                  </a>
                 </div>
+              </div>
             </div>
             <div className="card-body">
-                <h4 className="text-xl text-purple-700"><a href={post.link} className="post-title" rel="noreferrer" target="_blank">{post.title}</a></h4>
-                <h5 className="text-sky-600"><a href={post.link} className="post-title" rel="noreferrer" target="_blank">{post.pubDate}</a></h5>
-                <p className="card-text">{`${ToText(post.description.substring(0,300))}...`}</p>
+              <h4 className="text-xl text-purple-700">
+                <a
+                  href={post.link}
+                  className="post-title"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  {post.title}
+                </a>
+              </h4>
+              <h5 className="text-sky-600">
+                <a
+                  href={post.link}
+                  className="post-title"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  {post.pubDate}
+                </a>
+              </h5>
+              <p className="card-text">{`${ToText(
+                post.description.substring(0, 300)
+              )}...`}</p>
             </div>
-        </div>
+          </div>
+          <div
+            className="p-4 m-4 max-w-sm rounded-lg border border-zinc-500 dark:text-zinc-400 dark:bg-zinc-800 shadow-zinc-300 dark:shadow-zinc-600"
+            key={index}
+          >
+            <div className="flex items-center justify-center mb-8">
+              <div className="relative">
+                <img
+                  src={post.thumbnail}
+                  alt="Resim 1"
+                  className="h-56 w-full object-cover"
+                />
+                <div className="absolute bottom-0 left-0">
+                  <a
+                    href={profile.profileUrl}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      src={profile.profileImage}
+                      alt="Resim 2"
+                      className="w-11 h-11 rounded-full border-2 border-white -mb-3 ml-3"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="card-body">
+              <h4 className="text-xl text-purple-700">
+                <a
+                  href={post.link}
+                  className="post-title"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  {post.title}
+                </a>
+              </h4>
+              <h5 className="text-sky-600">
+                <a
+                  href={post.link}
+                  className="post-title"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  {post.pubDate}
+                </a>
+              </h5>
+              <p className="card-text">{`${ToText(
+                post.description.substring(0, 300)
+              )}...`}</p>
+            </div>
+          </div>
+        </>
       ));
     }
   };
   return (
     <div className="blogs">
       <h2>Blog Posts</h2>
-      <div className="container-fluid">
+      <div className="container-fluid flex">
         {blog.isLoading ? "Loading..." : haveBlogs()}
       </div>
     </div>
--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -1,5 +1,5 @@
 import { loremIpsum } from "react-lorem-ipsum";
-import React,{ useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Blog from "./Blog";
 import { use } from "i18next";
 export default function RightSide() {
@@ -16,24 +16,24 @@ export default function RightSide() {
     error: null,
   });
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch(mediumURL)
-    .then((res) => res.json())
-    .then((data) => {
-      const image=data.feed.image;
-      const link=data.feed.link;
-      const blogs=data.items;
-      const posts=blogs.filter((item)=>item.categories.length>-1);
+      .then((res) => res.json())
+      .then((data) => {
+        const image = data.feed.image;
+        const link = data.feed.link;
+        const blogs = data.items;
+        const posts = blogs.filter((item) => item.categories.length > 0);
 
-      setProfile({...profile, profileImage:image, profileUrl:link});
-      setBlog({...blog, item:posts, isLoading:false});
-    
-  })
-  .catch(err=>setBlog({...blog, error:err.message}))
-},[]);
+        setProfile({ ...profile, profileImage: image, profileUrl: link });
+        setBlog({ ...blog, item: posts, isLoading: false });
+      })
+      .catch((err) => setBlog({ ...blog, error: err.message }));
+  }, []);
   return (
     <div className="text-wrapper">
       <Blog blog={blog} profile={profile} />
+
       {loremIpsum({ p: 3 }).map((text, index) => (
         <div
           className="p-4 m-4 rounded-lg border border-zinc-500 dark:text-zinc-400 dark:bg-zinc-800 shadow-zinc-300 dark:shadow-zinc-600"
